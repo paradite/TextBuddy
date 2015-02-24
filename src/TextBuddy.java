b96d6b686725cb31fb24f97eb9821f4603cbb000
Zhu Liang <zhuliangg11@gmail.com>
2015-02-24 15:47:45 +0800

    modified behaviour for existing file, show no warning

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -12,20 +12,18 @@ import java.util.Scanner;
  * Assumption 1: The text file specified by the user could have already been
  * created and is not empty.
  * 
- * In this case, we show an warning to the user that the file may have been
- * created by other programs and the user should take caution when editing them.
- * We still proceed to read and write the file, assuming that the text file
- * follows the file format we use for TextBuddy.
+ * In this case, we do not show the warning to the user that the file may have
+ * been created by other programs. We still proceed to read and write the file,
+ * assuming that the text file follows the file format we use for TextBuddy.
  * 
  * Assumption 2: The text file specified by the user may not have been created.
  * 
  * In this case, we create an empty file with the specified name for the user.
  * 
- * @author paradite
+ * @author A0093910H
  *
  */
 public class TextBuddy {
-    private static Scanner scanner = new Scanner(System.in);
 
     /**
      * String constants
@@ -41,8 +39,7 @@ public class TextBuddy {
     private static final String ERROR_INVALID_COMMAND = "Invalid command";
     private static final String ERROR_INVALID_LINE_NUMBER = "Invalid line number";
 
-    private static final String WARNING_FILE_EXISTS = "Warning: The file already exists and it may not have been created by TextBuddy."
-            + " Please edit with caution.";
+    private static final String WARNING_FILE_EXISTS = "Warning: The file already exists, proceed with caution.";
 
     private static final String MESSAGE_WELCOME = "Welcome to TextBuddy. %1$s is ready for use";
     private static final String MESSAGE_WAIT_FOR_COMMAND = "command: ";
@@ -57,6 +54,7 @@ public class TextBuddy {
     private static final String MESSAGE_DELETE_SUCCESS = "deleted from %1$s: \"%2$s\"";
     private static final String MESSAGE_EMPTY_FILE = "%1$s is empty";
 
+    private static Scanner scanner = new Scanner(System.in);
     private static File file;
     private static String fileName;
     private static BufferedReader fileReader;
@@ -88,7 +86,7 @@ public class TextBuddy {
     }
 
     /**
-     * Main method to take user inputs and respond
+     * Method to take user inputs and respond
      */
     private static void readCommands() {
         while (scanner.hasNextLine()) {
@@ -99,6 +97,12 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to execute the command that the user provides
+     * 
+     * @param command
+     * @return The result of the execution as String
+     */
     private static String executeCommand(String command) {
         if (command.trim().isEmpty()) {
             return ERROR_INVALID_COMMAND;
@@ -120,6 +124,12 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method for exiting the program
+     * 
+     * @param command
+     * @return signal as String if the argument is not invalid
+     */
     private static String exitProgram(String command) {
         // Verify that the command does not have extra parameters
         String[] parameters = getParameters(command);
@@ -130,6 +140,12 @@ public class TextBuddy {
         return command;
     }
 
+    /**
+     * Method for handling delete operation
+     * 
+     * @param command
+     * @return signal for the operation as String
+     */
     private static String deleteOperation(String command) {
         String[] parameters = getParameters(command);
         if (parameters.length != 1) {
@@ -139,15 +155,19 @@ public class TextBuddy {
         return deleteLine(lineNumberToDelete);
     }
 
+    /**
+     * Method to delete the specified line of text
+     * 
+     * @param lineNumberToDelete
+     * @return signal for the operation as String
+     */
     private static String deleteLine(int lineNumberToDelete) {
         // Read the content, skipping the line, clear the content, then save the
         // new content
-        StringBuilder newContentBuilder = new StringBuilder();
         try {
             String deletedLineContent = getLineAt(lineNumberToDelete);
-            String newContent = readAndSkipLine(lineNumberToDelete,
-                    newContentBuilder);
-            recreateFile();
+            String newContent = readAndSkipLine(lineNumberToDelete);
+            isFileRecreated();
             writeNewContent(newContent);
 
             return String.format(MESSAGE_DELETE_SUCCESS, file,
@@ -158,6 +178,12 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to write new content into the file after deleting operation
+     * 
+     * @param newContent
+     * @throws IOException
+     */
     private static void writeNewContent(String newContent) throws IOException {
         setUpWriter();
         fileWriter.write(newContent);
@@ -165,6 +191,13 @@ public class TextBuddy {
         closeWriter();
     }
 
+    /**
+     * Method to get the content at a specific line as String
+     * 
+     * @param lineNumberToDelete
+     * @return the content of the line as String
+     * @throws IOException
+     */
     private static String getLineAt(int lineNumberToDelete) throws IOException {
         setUpReader();
         int lineNumber = 1;
@@ -181,8 +214,18 @@ public class TextBuddy {
         return ERROR_INVALID_LINE_NUMBER;
     }
 
-    private static String readAndSkipLine(int lineNumberToDelete,
-            StringBuilder newContentBuilder) throws IOException {
+    /**
+     * Method to read the text file line by line, skipping the line to be
+     * deleted and save the content as a String
+     * 
+     * @param lineNumberToDelete
+     * @param newContentBuilder
+     * @return the content of the text file without the specified line as String
+     * @throws IOException
+     */
+    private static String readAndSkipLine(int lineNumberToDelete)
+            throws IOException {
+        StringBuilder newContentBuilder = new StringBuilder();
         setUpReader();
         int lineNumber = 1;
         String lineString = fileReader.readLine();
@@ -203,6 +246,12 @@ public class TextBuddy {
         return newContentBuilder.toString();
     }
 
+    /**
+     * Method to clear the content of the file by recreating it
+     * 
+     * @param command
+     * @return Signal for the result of clearing operation as String
+     */
     private static String clearContent(String command) {
         // Verify that the command does not have extra parameters
         String[] parameters = getParameters(command);
@@ -210,7 +259,7 @@ public class TextBuddy {
             return ERROR_INVALID_COMMAND;
         }
         // Delete and recreate the file to clear the content
-        boolean success = recreateFile();
+        boolean success = isFileRecreated();
         if (success) {
             return String.format(MESSAGE_CLEAR_SUCCESS, fileName);
         } else {
@@ -219,7 +268,12 @@ public class TextBuddy {
 
     }
 
-    private static boolean recreateFile() {
+    /**
+     * Method to recreate the file
+     * 
+     * @return boolean signal for success or failure
+     */
+    private static boolean isFileRecreated() {
         try {
             file.delete();
             file.createNewFile();
@@ -230,6 +284,13 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to display the content of the text file
+     * 
+     * @param command
+     * @return the formatted content as String if argument is valid, otherwise
+     *         return the error signal as String
+     */
     private static String displayContent(String command) {
         String[] parameters = getParameters(command);
         // Make sure there are no extra parameters for display
@@ -239,6 +300,12 @@ public class TextBuddy {
         return formatContent();
     }
 
+    /**
+     * Method to get the formatted content of the file
+     * 
+     * @return the formatted content as String if the file is not empty,
+     *         otherwise, return the signal for empty file as String
+     */
     private static String formatContent() {
         String contentToDisplay;
         String lineContent;
@@ -258,6 +325,13 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to read the file line by line and format the content as required
+     * 
+     * @param lineContent
+     * @return formatted content as String
+     * @throws IOException
+     */
     private static String processContent(String lineContent) throws IOException {
         StringBuilder contentToDisplayBuilder = new StringBuilder();
         int lineNumber = 1;
@@ -267,8 +341,7 @@ public class TextBuddy {
                     lineContent);
             // Add new line before all lines except first one
             if (lineNumber > 1) {
-                contentToDisplayBuilder.append(System
-                        .getProperty("line.separator"));
+                contentToDisplayBuilder.append(System.lineSeparator());
             }
             contentToDisplayBuilder.append(lineContent);
             lineNumber++;
@@ -277,6 +350,12 @@ public class TextBuddy {
         return contentToDisplayBuilder.toString();
     }
 
+    /**
+     * Method to add a line of text to the file
+     * 
+     * @param command
+     * @return Signal for the adding operation as String
+     */
     private static String addText(String command) {
         String textToAdd = removeFirstWord(command).trim();
         try {
@@ -291,32 +370,63 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to write the end of file indicator for the text file
+     * 
+     * @throws IOException
+     */
     private static void writeEndOfFileIndicator() throws IOException {
         // Use an new line marker as end of file indicator
         fileWriter.newLine();
     }
 
+    /**
+     * Method to get the type of the command
+     * 
+     * @param command
+     * @return type of command as String
+     */
     private static String getCommandType(String command) {
         return getFirstWord(command);
     }
 
-    private static String removeFirstWord(String s) {
-        return s.replace(getFirstWord(s), "").trim();
+    /**
+     * Method to remove the first word of a String, without affecting the rest
+     * of the String
+     * 
+     * @param s
+     * @return the String with first word removed
+     */
+    public static String removeFirstWord(String s) {
+        return s.replaceFirst(getFirstWord(s), "").trim();
     }
 
-    private static String getFirstWord(String s) {
+    /**
+     * Method to get the first word of a String
+     * 
+     * @param s
+     * @return the first word of the String
+     */
+    public static String getFirstWord(String s) {
         String firstWord = s.trim().split("\\s+")[0];
         return firstWord;
     }
 
-    private static String[] getParameters(String command) {
+    /**
+     * Method to get the parameters in a command
+     * 
+     * @param command
+     * @return parameters as String array
+     */
+    public static String[] getParameters(String command) {
         String parameterString = removeFirstWord(command.trim());
         String[] parameters = parameterString.split("\\s+");
         return parameters;
     }
 
     /**
-     * Process the filename provided by user
+     * Method to process the filename provided by user. Create one if it does
+     * not already exist.
      * 
      * @param filename
      * @return File object to interact with
@@ -324,13 +434,13 @@ public class TextBuddy {
     private static File processFilename(String filename) {
         file = new File(filename);
         if (file.exists() && !file.isDirectory()) {
-            // Warn the user if the file is not empty
+            // Do not warn the user if the file is not empty
             try {
                 setUpReader();
                 String lineContent = fileReader.readLine();
-                if (lineContent != null) {
-                    displayMessageNewLine(WARNING_FILE_EXISTS);
-                }
+                // if (lineContent != null) {
+                // displayMessageNewLine(WARNING_FILE_EXISTS);
+                // }
                 closeReader();
             } catch (IOException e) {
                 displayMessageNewLine(ERROR_READING_WRITING);
@@ -352,10 +462,10 @@ public class TextBuddy {
     }
 
     /**
-     * Check if the argument supplied by the user is valid
+     * Method to check if the initial argument supplied by the user is valid
      * 
      * @param args
-     * @return
+     * @return valid or invalid as boolean
      */
     private static boolean isValidArgument(String[] args) {
         if (args.length == 0) {
@@ -370,25 +480,26 @@ public class TextBuddy {
     }
 
     /**
-     * Display a message with a new line to the user
+     * Method to display a message with a new line to the user
      * 
      * @param message
      */
     private static void displayMessageNewLine(String message) {
         System.out.println(message);
-
     }
 
     /**
-     * Display an inline message without new line to the user
+     * Method to display an inline message without new line to the user
      * 
      * @param message
      */
     private static void displayMessageInline(String message) {
         System.out.print(message);
-
     }
 
+    /**
+     * Method to set up the BufferedReader for reading the text file
+     */
     private static void setUpReader() {
         try {
             fileReader = new BufferedReader(new FileReader(file));
@@ -398,6 +509,9 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to close BufferedReader
+     */
     private static void closeReader() {
         try {
             fileReader.close();
@@ -407,6 +521,9 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to set up the BufferedWriter for writing into the text file
+     */
     private static void setUpWriter() {
         try {
             fileWriter = new BufferedWriter(new FileWriter(file, true));
@@ -416,6 +533,9 @@ public class TextBuddy {
         }
     }
 
+    /**
+     * Method to close BufferedWriter
+     */
     private static void closeWriter() {
         try {
             fileWriter.close();
